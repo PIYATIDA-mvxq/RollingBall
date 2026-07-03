1d3aace38042d7bee01bebba45948ec861a3ed91
PIYATIDA-mvxq <piyatida.jam@ku.th>
2026-07-03 15:58:05 +0700

    Add files via upload

---
+++ Lib/Rolling.java
@@ -0,0 +1,29 @@
+package Lib;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+
+public class Rolling extends JPanel implements ActionListener {
+    int x = 100;
+    int start_angle = 0 ;
+    public Rolling(){
+        Timer t = new Timer(50, this);
+        t.start();
+    }
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+         g.drawOval(x,90,80,80);
+         g.fillArc(x,90,80,80,start_angle,180);
+    }
+@Override
+public void actionPerformed(ActionEvent e){
+    x -= 2;
+    start_angle += 2;
+    if(x<-80) x=getWidth();
+    if(start_angle>=360) start_angle=0;
+        repaint();
+}
+    
+}
